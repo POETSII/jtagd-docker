91eae5d5bd2ca68dfa0d2d849ca7506c4eec0947
Theo Markettos <git+atm26@cl.cam.ac.uk>
2022-08-05 12:32:27 +0100

    Create script to run jtag stack and note which Quartus files are used

---
+++ run_jtag_listfiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Runs Quartus jtag stack and list which Quartus files/shared libraries are loaded
+# The list may differ depending on what JTAG deviecs you have plugged in.
+# Requires $QUARTUS_ROOTDIR to be set
+#
+# syntax: run_jtag_listfiles.sh <output_file_for_list>
+
+FILELIST=$1
+QUARTUSDIR=$(basename $QUARTUS_ROOTDIR)
+
+TRACEDIR=/tmp/jtag
+mkdir -p $TRACEDIR
+strace -f -s999 -o $TRACEDIR/stjtagd $QUARTUS_ROOTDIR/bin/jtagd --foreground --debug  > $TRACEDIR/jtagd.log &
+JTAGD=$!
+echo "JTAGD=$JTAGD"
+strace -f -s999 -o $TRACEDIR/stjtagconfig $QUARTUS_ROOTDIR/bin/jtagconfig > $TRACEDIR/jtagconfig.log
+kill %1
+
+grep open $TRACEDIR/stjtagd  | grep -v ENOENT | grep -v resumed | grep $QUARTUS_ROOTDIR | perl -lne "print \$2 if /($QUARTUSDIR\/(.*)\")/i" > $TRACEDIR/filelist.txt
+grep open $TRACEDIR/stjtagconfig  | grep -v ENOENT | grep -v resumed | grep $QUARTUS_ROOTDIR | perl -lne "print \$2 if /($QUARTUSDIR\/(.*)\")/i" >> $TRACEDIR/filelist.txt
+
+cat $TRACEDIR/filelist.txt | sort | uniq > $FILELIST
