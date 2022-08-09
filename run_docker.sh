bb521af38bc10d1e9fadd4a9ee56fd714484e037
Theo Markettos <atm26@cl.cam.ac.uk>
2022-08-09 17:37:46 +0100

    Add licence headers to various scripts, tidy

--- run_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#docker -D run -p $1:1309 jtagd
-DEVICE=$1
-QUARTUS_ROOTDIR=/local/ecad/altera/21.3pro/quartus ./build-docker.sh && \
-docker -D run --privileged -v /var/tmp:/sys/bus/usb \
-	-v /sys/bus/usb/devices/5-2.3.2/:/sys/bus/usb/devices/5-2.3.2 \
-	-v /dev/bus/usb:/dev/bus/usb \
-	jtagd
--- start_docker_multi.sh
+++ start_docker_multi.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-# cleanup via:
-#  docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2022 A. Theodore Markettos
+#
+# This software was developed by the University of Cambridge Computer
+# Laboratory as part of the CAPcelerate project, funded by EPSRC grant
+# EP/V000381/1.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
 
 JTAG_CONF=$HOME/.jtag.conf
 
 export QUARTUS_ROOTDIR=/local/ecad/altera/21.3pro/quartus
 
 echo "Making sure all boards have firmware programmed..."
-#$QUARTUS_ROOTDIR/jtagconfig
-#killall jtagd
-
+$QUARTUS_ROOTDIR/jtagconfig
+killall jtagd
 # now boards are 09fb:6010 not unprogrammed 09fb:6810, so we
 # don't need to worry about udev
 
